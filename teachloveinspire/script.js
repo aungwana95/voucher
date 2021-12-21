357499b5a04bd546a89903c2ea8664f6d1b16d8c
aungwana <59996144+aungwana95@users.noreply.github.com>
2021-12-21 14:19:15 +0630

    Add files via upload

---
+++ teachloveinspire/script.js
@@ -0,0 +1,45 @@
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext('2d')
+//---INPUT---//
+const nameInput = document.getElementById('name')
+const nrcInput = document.getElementById('nrc')
+const classInput = document.getElementById('class')
+const dateInput = document.getElementById('date')
+const cidInput = document.getElementById('cid')
+
+const downloadBtn = document.getElementById('download-btn')
+
+const image = new Image()
+image.src = 'certificate.jpg'
+image.onload = function () {
+	drawImage()
+}
+
+function drawImage() {
+	// ctx.clearRect(0, 0, canvas.width, canvas.height)
+	ctx.drawImage(image, 0, 0, canvas.width, canvas.height)
+	ctx.font = '20px Gadugi bold'
+	ctx.fillStyle = '#000000'
+
+//---NAME---//
+	ctx.fillText(nameInput.value, 95, 240)
+//---NAME---//
+	ctx.fillText(nrcInput.value, 40, 350)
+//---CLASS---//
+        ctx.fillText(classInput.value, 380, 350)
+        ctx.fillText(classInput.value, 380, 400)
+//---DATE---//
+        ctx.fillText(dateInput.value, 360, 240)
+//---CID---//
+        ctx.fillText(cidInput.value, 95, 270)
+}
+
+
+nameInput.addEventListener('input', function () {
+	drawImage()
+})
+
+downloadBtn.addEventListener('click', function () {
+	downloadBtn.href = canvas.toDataURL('image/jpg')
+	downloadBtn.download = 'voucher' + nameInput.value
+})
