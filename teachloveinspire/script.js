b593babe204e185e8fe2cbd6efa67c364cfaea67
aungwana <59996144+aungwana95@users.noreply.github.com>
2021-12-25 10:53:37 +0630

    Update script.js

--- teachloveinspire/script.js
+++ teachloveinspire/script.js
@@ -30,12 +30,18 @@ function drawImage() {
         ctx.fillText(classInput.value, 380, 400)
 //---DATE---//
         ctx.fillText(dateInput.value, 360, 240)
-//---CID---//
-        ctx.fillText(cidInput.value, 95, 270)
 }
 
-
 nameInput.addEventListener('input', function () {
+	drawImage()
+})
+nrcInput.addEventListener('input', function () {
+	drawImage()
+})
+classInput.addEventListener('input', function () {
+	drawImage()
+})
+dateInput.addEventListener('input', function () {
 	drawImage()
 })
 
