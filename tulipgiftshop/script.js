0aa5be39d602def67d41d28dcbd8c1294a3656ec
aungwana <59996144+aungwana95@users.noreply.github.com>
2021-12-25 10:57:17 +0630

    Update script.js

--- tulipgiftshop/script.js
+++ tulipgiftshop/script.js
@@ -34,8 +34,16 @@ function drawImage() {
         ctx.fillText(cidInput.value, 500, 660)
 }
 
-
 nameInput.addEventListener('input', function () {
+	drawImage()
+})
+nrcInput.addEventListener('input', function () {
+	drawImage()
+})
+classInput.addEventListener('input', function () {
+	drawImage()
+})
+dateInput.addEventListener('input', function () {
 	drawImage()
 })
 
